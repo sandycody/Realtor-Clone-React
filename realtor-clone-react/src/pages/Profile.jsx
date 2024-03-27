55809274fc9b0398be187c250bb460df3235d850
Sandeep <sandeepwadhawan2001@gmail.com>
2024-03-27 16:27:00 +0530

    Added the edit functionality tothe profile page

--- realtor-clone-react/src/pages/Profile.jsx
+++ realtor-clone-react/src/pages/Profile.jsx
@@ -1,20 +1,53 @@
 import React, { useState } from 'react';
-import { getAuth, signOut } from "firebase/auth";
+import { getAuth, signOut, updateProfile } from "firebase/auth";
 import { useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
+import { doc, updateDoc } from 'firebase/firestore';
+import { db } from '../firebase';
 
 function Profile() {
   const auth = getAuth();
   const navigate = useNavigate();
+  const [changeDetail, setChangeDetail] = useState(false);
+
   const [formData, setFormData] = useState({
     name: auth.currentUser.displayName,
     email: auth.currentUser.email
   });
 
   const { name, email } = formData; //Object destructuring
+
   function onLogOut() {
-    auth.signOut();
+    signOut(auth);
     navigate('/');
   }
+
+  function onChange(e) {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.id]: e.target.value
+    }));
+  }
+
+  async function onSubmit() {
+    try {
+      if(auth.currentUser.displayName !== name) {
+        // if name is changed, then Update the display name in firebase authentication
+        await updateProfile(auth.currentUser, {
+          displayName: name
+        });
+        // Update the name in the firestore too
+        const docRef = doc(db, "users", auth.currentUser.uid);
+        await updateDoc(docRef, {
+          name
+        });
+      } 
+      toast.success("Profile details updated");
+    } catch (error) {
+      toast.error("Could not update the profile details");
+    }
+  }
+
   return (
     <>
       <section className='max-w-6xl mx-auto flex justify-center items-center flex-col'>
@@ -23,15 +56,35 @@ function Profile() {
           <form>
             {/* Name Input */}
 
-            <input type="text" id="name" value={name} placeholder='Name' disabled className='w-full px-4 py-2 mb-6 text-xl text-gray-700 bg-white border border-gray-300 rounded transition ease-in-out' />
+            <input
+              type="text"
+              id="name"
+              value={name}
+              placeholder='Name'
+              disabled={!changeDetail}
+              onChange={onChange}
+              className={`w-full px-4 py-2 mb-6 text-xl text-gray-700 bg-white border border-gray-300 rounded transition ease-in-out ${changeDetail && "bg-red-200 focus:bg-red-200"}`}
+            />
 
             {/* Email Input */}
 
-            <input type="email" id="email" value={email} placeholder='Email Address' disabled className='w-full px-4 py-2 mb-6 text-xl text-gray-700 bg-white border border-gray-300 rounded transition ease-in-out' />
+            <input
+              type="email"
+              id="email"
+              value={email}
+              placeholder='Email Address'
+              disabled
+              className='w-full px-4 py-2 mb-6 text-xl text-gray-700 bg-white border border-gray-300 rounded transition ease-in-out'
+            />
 
             <div className="flex justify-between whitespace-nowrap text-sm sm:text-lg mb-6">
               <p className="flex items-center">Do you want to change your name?
-                <span className='ml-1 text-red-600 hover:text-red-700 transition duration-200 ease-in-out cursor-pointer'>Edit</span>
+                <span onClick={() => {
+                  changeDetail && onSubmit();
+                  setChangeDetail((prevState) => !prevState)
+                }} 
+                  className='ml-1 text-red-600 hover:text-red-700 transition duration-200 ease-in-out cursor-pointer'>{changeDetail ? "Apply Change" : "Edit"}
+                </span>
               </p>
               <p onClick={onLogOut} className='text-blue-600 hover:text-blue-800 transition duration-200 ease-in-out cursor-pointer'>Sign Out</p>
             </div>
